69f7ef015fcdd6347afd5202b3b413b3cb2d6d92
Jromary <julienromary@yahoo.fr>
2019-01-16 14:19:08 +0100

    Yal0 : Jtest

---
+++ Yal0/src/yal/test/simulateur/mainSimulateur.java
@@ -0,0 +1,10 @@
+package yal.test.simulateur;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class mainSimulateur {
+    public static void main(String[] args) {
+
+    }
+}
