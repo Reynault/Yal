632e659e57bc90870bfe6b41c528b0a05dbbc8e6
Jromary <julienromary@yahoo.fr>
2019-01-16 14:24:44 +0100

    Yal0 : Jtest_01

--- Yal0/src/yal/test/simulateur/mainSimulateur.java
+++ Yal0/src/yal/test/simulateur/mainSimulateur.java
@@ -1,10 +1,15 @@
 package yal.test.simulateur;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import yal.Yal;
+
+import java.io.File;
 
 public class mainSimulateur {
     public static void main(String[] args) {
+        int nbFiles = new File("sources/").listFiles().length;
+        for (int i = 0; i < nbFiles; i++) {
+            Yal.main(new String[]{"test" + i + ".yal"});
+        }
 
     }
 }
