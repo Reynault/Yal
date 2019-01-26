37cc2e8d58f25742b33cafe64f1eead1c4223c5b
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-01-26 15:22:00 +0100

    Yal1 : Modification des tests pour le simulateur. Ajout de la classe Programme.

--- Yal1/src/yal/arbre/BlocDInstructions.java
+++ Yal1/src/yal/arbre/BlocDInstructions.java
@@ -11,24 +11,9 @@ import java.util.ArrayList;
  */
 
 public class BlocDInstructions extends ArbreAbstrait {
-    // Liste des instructions du programme
+    // Liste des instructions du bloc
     protected ArrayList<ArbreAbstrait> programme ;
-    // Zone du code qui stocke les données utilisées
-    protected static String zoneData = "# Code généré par Yal\n" +
-            ".data\n" +
-            "# Caractère de fin de ligne\n" +
-                                            "finLigne:     .asciiz \"\\n\"\n" +
-                                            "              .align 2\n" ;
-    // Début du programme
-    protected static String debutCode = "# Début du programme\n" +
-            ".text\n" +
-            "main :\n" +
-            "\t# Initialisation de s7 avec sp\n" +
-            "\tmove $s7, $sp\n" ;
-    // Fin du programme
-    protected static String finCode = "end :\n" +
-                                      "    li $v0, 10\n" +
-                                      "    syscall\n" ;
+
     /**
      * Constructeur
      * @param n numéro de la ligne
@@ -72,16 +57,9 @@ public class BlocDInstructions extends ArbreAbstrait {
     @Override
     public String toMIPS() {
         StringBuilder sb = new StringBuilder("") ;
-        sb.append(zoneData) ;
-        sb.append(debutCode) ;
-        // On initialise toutes les variables utilisées
-        TDS instance = TDS.getInstance();
-        sb.append("\t# Réservation de l'espace dans la pile\n");
-        sb.append("\taddi $sp, $sp, "+instance.getPeak()+"\n");
         for (ArbreAbstrait a : programme) {
             sb.append(a.toMIPS()) ;
         }
-        sb.append(finCode) ;
         return sb.toString() ;
     }
 
