f64878f09e22c1f5080a89128f1bea9ce7a158ed
sies2u <reynault.sies8@etu.univ-lorraine.fr>
2019-02-13 14:59:31 +0100

    Yal3 : Mise à jour du gestionnaire nombres

--- Yal3/src/yal/arbre/GestionnaireNombres.java
+++ Yal3/src/yal/arbre/GestionnaireNombres.java
@@ -1,18 +1,31 @@
 package yal.arbre;
 
+/**
+ * Class singleton qui gère l'incrémentation des nombres utilisés
+ * pour les structures mips (labels) et pour le nombre de blocs
+ */
 public class GestionnaireNombres {
 
     private int num_condition;
     private int num_iteration;
     private int num_expression;
     private int num_ecrire;
+    private int compteur_blocs;
 
+    /**
+     * Instance unique
+     */
     private static GestionnaireNombres instance = new GestionnaireNombres();
+
+    /**
+     * Constructeur
+     */
     private GestionnaireNombres(){
         num_condition = 0;
         num_expression = 0;
         num_iteration = 0;
         num_ecrire = 0;
+        compteur_blocs = 0;
     }
     public static GestionnaireNombres getInstance(){
         return instance;
@@ -36,5 +49,11 @@ public class GestionnaireNombres {
     public int nouvelleEcriture(){
         num_ecrire++;
         return num_ecrire;
+    }
+
+    public int nouveau_bloc(){
+        int i = compteur_blocs;
+        compteur_blocs ++;
+        return i;
     }
 }
